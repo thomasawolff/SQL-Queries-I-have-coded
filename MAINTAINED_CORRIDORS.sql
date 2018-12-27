44a5765ca8e955224835ffd3efc597e40ee4e574
Thomas Wolff <twolff@mt.gov>
2018-12-26 19:11:55 -0700

    Add files via upload

---
+++ MAINTAINED_CORRIDORS.sql
@@ -0,0 +1,20 @@
+select t.nrlg_dept_route,dt.begmile,dt.endmile
+from TIS.TIS_NEW_ROADLOG t
+inner join (select d.nrlg_dept_route as corridor,
+           min(d.nrlg_dept_length_mi) as begmile,
+           sum(d.nrlg_dept_length_mi) as endmile
+           from TIS.TIS_NEW_ROADLOG d 
+           group by d.nrlg_dept_route) dt
+on t.nrlg_dept_route = dt.corridor
+where t.nrlg_dept_route in 
+(select s.nrlg_dept_route from TIS.TIS_NEW_ROADLOG s where s.nrlg_smaint like 'Y') 
+and t.nrlg_sys_desc is not null
+and t.nrlg_sys_desc not in (select t.nrlg_sys_desc from TIS.TIS_NEW_ROADLOG t
+                         where t.nrlg_sys_desc = 'OFF' or t.nrlg_sys_desc = 'OUT' or t.nrlg_sys_desc = 'CLO'
+                         group by t.nrlg_sys_desc)
+and t.nrlg_srf_type in (select t.nrlg_srf_type from TIS.TIS_NEW_ROADLOG t where t.nrlg_srf_type = 'PMS'
+                         or t.nrlg_srf_type = 'PCC' or t.nrlg_srf_type = 'BST' or t.nrlg_srf_type = 'RMS'
+                         group by t.nrlg_srf_type) 
+and t.nrlg_dept_route in (select e.corridor_code as corridor from sec_segments e where e.secfile_name like 'North' group by e.corridor_code)
+group by t.nrlg_dept_route,dt.begmile,dt.endmile
+order by 1
