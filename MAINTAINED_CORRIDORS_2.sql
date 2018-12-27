654a7f03ee139316832d19646c049732abd87639
Thomas Wolff <twolff@mt.gov>
2018-12-27 10:17:20 -0700

    Add files via upload

--- MAINTAINED_CORRIDORS_2.sql
+++ MAINTAINED_CORRIDORS_2.sql
@@ -1,25 +1,18 @@
-select t.nrlg_dept_route,dt.begmile,dt.endmile
+select t.nrlg_dept_route||t.nrlg_dept_roadbed,dt.begmile,dt.endmile
 from TIS.TIS_NEW_ROADLOG t
-inner join (select d.nrlg_dept_route as corridor,
+inner join (select d.nrlg_dept_route||d.nrlg_dept_roadbed as corridor,
            min(d.nrlg_dept_length_mi) as begmile,
            sum(d.nrlg_dept_length_mi) as endmile
            from TIS.TIS_NEW_ROADLOG d 
-           group by d.nrlg_dept_route) dt
-on t.nrlg_dept_route = dt.corridor
-where t.nrlg_dept_route in 
-(select s.nrlg_dept_route from TIS.TIS_NEW_ROADLOG s where s.nrlg_smaint like 'Y') 
-and t.nrlg_sys_desc is not null and t.nrlg_smaint is not null
-and t.nrlg_sys_desc not in (select t.nrlg_sys_desc from TIS.TIS_NEW_ROADLOG t
+           where d.nrlg_sys_desc not in (select t.nrlg_sys_desc from TIS.TIS_NEW_ROADLOG t
                          where t.nrlg_sys_desc = 'OFF' or t.nrlg_sys_desc = 'OUT' or t.nrlg_sys_desc = 'CLO'
                          group by t.nrlg_sys_desc)
-and t.nrlg_srf_type in (select t.nrlg_srf_type from TIS.TIS_NEW_ROADLOG t where t.nrlg_srf_type = 'PMS'
+           and d.nrlg_srf_type in (select t.nrlg_srf_type from TIS.TIS_NEW_ROADLOG t where t.nrlg_srf_type = 'PMS'
                          or t.nrlg_srf_type = 'PCC' or t.nrlg_srf_type = 'BST' or t.nrlg_srf_type = 'RMS'
                          group by t.nrlg_srf_type) 
-and t.nrlg_dept_route in (select s.nrlg_dept_route from TIS.TIS_NEW_ROADLOG s 
-                            where s.nrlg_dept_route in (select corridor_code from sec_segments t inner join TIS.TIS_NEW_ROADLOG s
-                            on s.nrlg_dept_route = t.corridor_code
-                            where t.secfile_name = 'North'
-                            group by t.corridor_code)
-                            group by s.nrlg_dept_route)
-group by t.nrlg_dept_route,dt.begmile,dt.endmile
+           and d.nrlg_smaint like 'Y'
+           group by d.nrlg_dept_route||d.nrlg_dept_roadbed) dt
+on t.nrlg_dept_route||t.nrlg_dept_roadbed = dt.corridor 
+where t.nrlg_sys_desc is not null and t.nrlg_smaint is not null
+group by t.nrlg_dept_route||t.nrlg_dept_roadbed,dt.begmile,dt.endmile
 order by 1
