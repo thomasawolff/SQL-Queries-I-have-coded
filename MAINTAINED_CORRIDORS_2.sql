3b9a5b04bcd4523901f219c590523fa5b68320fb
Thomas Wolff <twolff@mt.gov>
2018-12-31 13:38:49 -0700

    Update MAINTAINED_CORRIDORS_2.sql

--- MAINTAINED_CORRIDORS_2.sql
+++ MAINTAINED_CORRIDORS_2.sql
@@ -1,4 +1,4 @@
-select t.nrlg_dept_route||t.nrlg_dept_roadbed,dt.begmile,dt.endmile
+           select t.nrlg_dept_route||t.nrlg_dept_roadbed,dt.begmile,dt.endmile
 from TIS.TIS_NEW_ROADLOG t
 inner join (select d.nrlg_dept_route||d.nrlg_dept_roadbed as corridor,d.nrlg_smaint as maintain,
            min(d.nrlg_dept_length_mi) as begmile,
@@ -11,7 +11,7 @@ inner join (select d.nrlg_dept_route||d.nrlg_dept_roadbed as corridor,d.nrlg_sma
                          or t.nrlg_srf_type = 'PCC' or t.nrlg_srf_type = 'BST' or t.nrlg_srf_type = 'RMS'
                          group by t.nrlg_srf_type) 
            and d.nrlg_dept_route in (select f.nrlg_dept_route from TIS.TIS_NEW_ROADLOG f 
-           where f.nrlg_smaint like 'Y' group by f.nrlg_dept_route)
+                                     where f.nrlg_smaint like 'Y' group by f.nrlg_dept_route)
            group by d.nrlg_dept_route||d.nrlg_dept_roadbed,d.nrlg_smaint) dt
 on t.nrlg_dept_route||t.nrlg_dept_roadbed = dt.corridor and dt.maintain = t.nrlg_smaint
 where t.nrlg_sys_desc is not null and 
